06fa9a75f1af3186bcc1319dfefcf2f16a746ab4
fulder <misad90@gmail.com>
2020-06-21 15:31:39 +0200

    Add pre/post hooks

--- renew.py
+++ renew.py
@@ -3,6 +3,8 @@ import os
 import sys
 from subprocess import Popen
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
 
 def main():
     args = _get_args()
@@ -15,14 +17,14 @@ def _get_args():
     parser = argparse.ArgumentParser(description='Cert Renewal')
     parser.add_argument('--domain', "-d", required=True, help="Domain name to renew")
     parser.add_argument('--certbot-folder', required=False, help="Path to certbot out folder",
-                        default="/etc/letsencrypt/live")
+                        default="/etc/letsencrypt")
     return parser.parse_args()
 
 
 def _run_certbot(domain, certbot_folder):
-    config_dir = os.path.join(certbot_folder, domain, "config")
-    work_dir = os.path.join(certbot_folder, domain, "work")
-    logs_dir = os.path.join(certbot_folder, domain, "logs")
+    config_dir = os.path.join(certbot_folder, "config")
+    work_dir = os.path.join(certbot_folder, "work")
+    logs_dir = os.path.join(certbot_folder, "logs")
 
     p = Popen([
         "certbot", "certonly",
@@ -31,13 +33,16 @@ def _run_certbot(domain, certbot_folder):
         "--manual",
         "--config-dir", config_dir,
         "--work-dir", work_dir,
-        "--logs-dir", logs_dir
+        "--logs-dir", logs_dir,
+        "--staging",
+        "--manual-auth-hook", os.path.join(CURRENT_DIR, "pre_hook.py"),
+        "--manual-cleanup-hook", os.path.join(CURRENT_DIR, "post_hook.py"),
     ])
     p.communicate()
 
 
 def _create_pfx(domain, certbot_folder):
-    domain_folder = os.path.join(certbot_folder, domain)
+    domain_folder = os.path.join(certbot_folder, "live", domain)
     pfx_path = os.path.join(domain_folder, "certificate.pfx")
     privkey_path = os.path.join(domain_folder, "privkey.pem")
     fullchain_path = os.path.join(domain_folder, "fullchain.pem")
