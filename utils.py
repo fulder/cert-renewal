12a0ed13b1954d6d0fef59060176dd090ee7a2ab
fulder <misad90@gmail.com>
2020-06-21 16:41:34 +0200

    Create cp_or_scp helper func

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import re
+import shutil
 
 from paramiko import SSHClient
 from scp import SCPClient
@@ -15,3 +17,16 @@ def scp(file_path, username, ip, port, remote_path):
     scp = SCPClient(ssh.get_transport())
     scp.put(file_name, remote_path=remote_path)
     scp.close()
+
+
+def cp_or_scp(file_path, path):
+    m = re.match(r"(\w+)@([^:]+):(\d+):(.+)", path)
+    if m:
+        username = m.group(1)
+        ip = m.group(2)
+        port = int(m.group(3))
+        remote_path = m.group(4)
+
+        scp(file_path, username, ip, port, remote_path)
+    else:
+        shutil.copy2(file_path, path)
