428d8ed25ac8bf2898dde535ba75b0487a01c96b
fulder <misad90@gmail.com>
2020-06-21 15:19:10 +0200

    Make certbot folder configurable

--- renew.py
+++ renew.py
@@ -3,29 +3,41 @@ import os
 import sys
 from subprocess import Popen
 
-CERTBOT_FOLDER = os.path.join("/", "etc", "letsencrypt", "live")
-
 
 def main():
     args = _get_args()
 
-    _run_certbot(args.domain)
-    _create_pfx(args.domain)
+    _run_certbot(args.domain, args.certbot_folder)
+    _create_pfx(args.domain, args.certbot_folder)
 
 
 def _get_args():
     parser = argparse.ArgumentParser(description='Cert Renewal')
     parser.add_argument('--domain', "-d", required=True, help="Domain name to renew")
+    parser.add_argument('--certbot-folder', required=False, help="Path to certbot out folder",
+                        default="/etc/letsencrypt/live")
     return parser.parse_args()
 
 
-def _run_certbot(domain):
-    p = Popen(["certbot", "certonly", "--preferred-challenges", "http", "-d", domain, "--manual"])
+def _run_certbot(domain, certbot_folder):
+    config_dir = os.path.join(certbot_folder, domain, "config")
+    work_dir = os.path.join(certbot_folder, domain, "work")
+    logs_dir = os.path.join(certbot_folder, domain, "logs")
+
+    p = Popen([
+        "certbot", "certonly",
+        "--preferred-challenges", "http",
+        "-d", domain,
+        "--manual",
+        "--config-dir", config_dir,
+        "--work-dir", work_dir
+        "--logs-dir", logs_dir
+    ])
     p.communicate()
 
 
-def _create_pfx(domain):
-    domain_folder = os.path.join(CERTBOT_FOLDER, domain)
+def _create_pfx(domain, certbot_folder):
+    domain_folder = os.path.join(certbot_folder, domain)
     pfx_path = os.path.join(domain_folder, "certificate.pfx")
     privkey_path = os.path.join(domain_folder, "privkey.pem")
     fullchain_path = os.path.join(domain_folder, "fullchain.pem")
